c77a52a77fce78bd3ca8c4d73373c1719d836ace
wtei7 <m45a1@dgsw.hs.kr>
2025-06-23 12:12:08 +0900

    inital
    이거 되요?

---
+++ src/main/java/com/dgsw/skew/SkewApplication.java
@@ -0,0 +1,13 @@
+package com.dgsw.skew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dgsw/skew/SkewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgsw.skew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
