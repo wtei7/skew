439cfb60054781b58b12cd86d61ff82ada774260
wtei7 <m45a1@dgsw.hs.kr>
2025-06-24 11:30:30 +0900

    Feat :: 빈 컨트롤러 추가
    컨트롤러 추가(내부 서비스 구현 필요)

--- src/main/java/com/dgsw/skew/controller/SkewController.java
+++ src/main/java/com/dgsw/skew/controller/SkewController.java
@@ -4,7 +4,8 @@ package com.dgsw.skew.controller;
 import com.dgsw.skew.service.SkewService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -12,5 +13,38 @@ public class SkewController {
     @Autowired
     private SkewService skewService;
 
+    @GetMapping("/todos")
+    public ResponseEntity<String> getTodos() {
+        return ResponseEntity.ok("");
+    }
 
+    @PostMapping("/todos")
+    public ResponseEntity<String> makeTodos() {
+        return ResponseEntity.ok("");
+    }
+
+    @GetMapping("/todos/{id}")
+    public ResponseEntity<String> getTodo() {
+        return ResponseEntity.ok("");
+    }
+
+    @PutMapping("/todos/{id}")
+    public ResponseEntity<String> editTodo() {
+        return ResponseEntity.ok("");
+    }
+
+    @DeleteMapping("/todos/{id}")
+    public ResponseEntity<String> delTodo() {
+        return ResponseEntity.ok("");
+    }
+
+    @PatchMapping("/todos/order")
+    public ResponseEntity<String> reorderTodo() {
+        return ResponseEntity.ok("");
+    }
+
+    @PatchMapping("/todos/{id}/toggle")
+    public ResponseEntity<String> toggleTodo() {
+        return ResponseEntity.ok("");
+    }
 }
